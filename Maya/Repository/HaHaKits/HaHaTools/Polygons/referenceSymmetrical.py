6a5935d8011e04e1d83ffc5f81e545b6d1e2b18a
Chris Su <crystalsu2008@gmail.com>
2016-11-28 15:35:44 +0800

    Regular Update

--- Maya/Repository/HaHaKits/HaHaTools/Polygons/referenceSymmetrical.py
+++ Maya/Repository/HaHaKits/HaHaTools/Polygons/referenceSymmetrical.py
@@ -16,44 +16,40 @@ def symertricalOptions(across='YZ', posiToNega=True):
 
     return symId, symDir
 
-def matchPoints(referenceMesh, across='YZ', posiToNega=True):
-    symId, symDir = symertricalOptions(across='YZ', posiToNega=True)
+def referenceSymmetrical(across='YZ', posiToNega=False):
+    objs = pm.ls(sl=True, fl=True)
+    referenceMesh = objs[-1]
+    try:
+        refMeshShape = pm.listRelatives(referenceMesh, s=True)[0]
+    except:
+        print 'Error: The last selected object should be a referenceMesh.'
+        return
+    if not pm.objectType(refMeshShape, i='mesh'):
+        print 'Error: The last selected object should be a referenceMesh.'
+        return
 
-    refTrans = pm.listRelatives(referenceMesh, p=True)[0]
-    #piv = dt.Point(pm.xform(refTrans, q=True, ws=True, a=True, piv=True)[:3])
+    symId, symDir = symertricalOptions(across, posiToNega)
 
     matcher = pm.nodetypes.ClosestPointOnMesh()
     pm.connectAttr(referenceMesh+'.outMesh', matcher+'.inMesh')
 
-    machMap={}
-    vtxs = pm.ls(pm.polyListComponentConversion( referenceMesh, tv=True, internal=True ), fl=True)
-    for vtx in vtxs:
-        inpos = dt.Point(pm.pointPosition(vtx, l=True))
-        if inpos[symId]*symDir > 0:
-            inpos[symId] *= -1
+    for vtx in objs[:-1]:
+        vtxtype = pm.ls(vtx, showType=True)[1]
+        if vtxtype == 'float3':
+            meshvtx, vid = vtx.split('[')
+            vid = vid[:-1]
+            refvtx = referenceMesh+'.vtx['+vid+']'
 
-            pm.setAttr(matcher+'.inPosition', inpos)
-            symvid = str( pm.getAttr(matcher+'.closestVertexIndex') )
+            inpos = dt.Point(pm.pointPosition(refvtx, l=True))
+            if inpos[symId]*symDir > 0:
+                inpos[symId] *= -1
+                pm.setAttr(matcher+'.inPosition', inpos)
+                symvtxid = str( pm.getAttr(matcher+'.closestVertexIndex') )
 
-            vid = vtx.split('[')[1][:-1]
-            if not vid == symvid:
-                machMap[vid] = symvid
-                #pm.select(referenceMesh+'.vtx['+vid+']', tgl=True)
+                if not symvtxid == vid:
+                    symvtx = meshvtx+'['+symvtxid+']'
+                    pos = dt.Point(pm.pointPosition(symvtx, l=True))
+                    pos[symId] *= -1
+                    pm.xform(vtx, os=True, a=True, t=pos)
+
     pm.delete(matcher)
-
-    return machMap
-
-def referenceSymmetrical(across='YZ', posiToNega=False):
-    objs = pm.ls(sl=True, dag=True, type='mesh', fl=True)
-    referenceMesh = objs[-1]
-
-    symId, symDir = symertricalOptions(across, posiToNega)
-    machMap = matchPoints(referenceMesh, across, posiToNega)
-
-    for mesh in objs[1:]:
-        for vid, symvid in machMap.iteritems():
-            vtx = mesh+'.vtx['+vid+']'
-            symvtx = mesh+'.vtx['+symvid+']'
-            pos = dt.Point(pm.pointPosition(symvtx, l=True))
-            pos[symId] *= -1
-            pm.xform(vtx, os=True, a=True, t=pos)
